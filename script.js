d929baed5a85dff12291245130fa93dc0d5f9b03
xkj123456 <103395224+xkj123456@users.noreply.github.com>
2022-04-28 20:13:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/* Applying the "open" class when you click the Menu button, which triggers the mobile menu to open/close; Toggle Menu/Close menu text. */
+$(document).ready(function() {
+  $('.mobile-button a').click(function(e) {
+	$(this).parent().parent().toggleClass('open');
+  $(this).html($(this).html() == 'Close Menu' ? 'Menu' : 'Close Menu');
+    e.preventDefault();
+  });
+});
+<script> 
+function validateform(){ 
+var name=document.myform.name.value; 
+var name2=document.myform.name2.value; 
+var name3=document.myform.name3.value; 
+var Phone=document.myform.Phone.value; 
+ 
+if (name==null || name==""){ 
+  alert("First Name can't be blank"); 
+  return false; 
+}if (name2==null || name2==""){ 
+  alert("Last Name can't be blank"); 
+  return false;
+}if (name3==null || name3==""){ 
+  alert("Mailbox can't be blank"); 
+  return false;
+}else if(Phone.length<11){
+  alert("Phone must be at least 11 characters long.");
+  return false;
+  }
+}
+</script> 
